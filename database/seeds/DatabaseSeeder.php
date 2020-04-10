49fa38d67e8f77dc648e924dd05df8de74f74149
Abdukhaligov <hikmat.pou@gmail.com>
2020-04-11 01:36:55 +0400

    PresenterSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,6 +28,8 @@ class DatabaseSeeder extends Seeder {
     $this->call(SingleNewsSeeder::class);
     $this->call(SingleCabinetSeeder::class);
     $this->call(InitialSeeder::class);
+
+    $this->call(PresenterSeeder::class);
   }
 
 }
