f95ddb31a7ee81a20866c1cce151a4926b1f2afb
Abdukhaligov <hikmat.pou@gmail.com>
2020-04-10 10:18:53 +0400

    Update SingleHomeSeeder.php

--- database/seeds/SingleHomeSeeder.php
+++ database/seeds/SingleHomeSeeder.php
@@ -5,8 +5,49 @@ use Illuminate\Database\Seeder;
 class SingleHomeSeeder extends Seeder {
 
   public function run() {
+
+    $banners = [
+        [
+            "key" => "815963c423a05bed",
+            "layout" => "banners",
+            "attributes" => [
+                "img" => "banners/banner-bg-1.1.jpg",
+                "title" => "Best IT Soluations <br>Provider Agency",
+                "category" => "IT Business Consulting",
+                "subtitle" => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremda tium totam rem aperiam eaque ipsa quae ab illo inventore veritatis",
+                "btn_title" => "Google <i class=\"fal fa-long-arrow-right\"></i>",
+                "btn_link" => "https://google.com",
+            ]
+        ],
+        [
+            "key" => "78731e9e2e534626",
+            "layout" => "banners",
+            "attributes" => [
+                "img" => "banners/banner-bg-1.2.jpg",
+                "title" => "Best IT Soluations <br>Provider Agency",
+                "category" => "IT Business Consulting",
+                "subtitle" => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremda tium totam rem aperiam eaque ipsa quae ab illo inventore veritatis",
+                "btn_title" => "Google <i class=\"fal fa-long-arrow-right\"></i>",
+                "btn_link" => "https://google.com",
+            ]
+        ],
+        [
+            "key" => "9872053206c8274a",
+            "layout" => "banners",
+            "attributes" => [
+                "img" => "banners/banner-bg-1.3.jpg",
+                "title" => "Best IT Soluations <br>Provider Agenc",
+                "category" => "IT Business Consulting",
+                "subtitle" => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremda tium totam rem aperiam eaque ipsa quae ab illo inventore veritatis",
+                "btn_title" => "Google <i class=\"fal fa-long-arrow-right\"></i>",
+                "btn_link" => "https://google.com",
+            ]
+        ]
+    ];
+
     $data = [[
-        "title" => "{\"en\":\"Home\",\"ru\":\"Главная\"}"
+        "title" => json_encode(["en" => "Home","ru" => "Главная"]),
+        "banners" => json_encode($banners)
     ]];
 
     DB::table('single_homes')->insert($data);
