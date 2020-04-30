b95004a5893332955e026c2e1ad2aacd708cd6bd
Abdukhaligov <hikmat.pou@gmail.com>
2020-04-30 10:03:12 +0400

    fix logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,10 +78,6 @@
           <h2 class="nav-brand">
             <a href="/" style="margin: 0; padding: 0">
             <img class="top-logo"
-                  style="width: 85px;
-                  height: auto;
-                  margin-top: -12px;
-                  margin-bottom: -15px;" 
                   src="{{ Storage::disk('public')->url(\App\Models\Pages\Initial::getData()->logo) }}">
                 </a></h2>
           <!-- Sample menu definition -->
