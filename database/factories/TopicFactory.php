b6dcc9327a91f0f7a02c2cdecd8b4e5505e7b20d
Abdukhaligov <hikmat.pou@gmail.com>
2020-04-30 14:43:36 +0400

    Update TopicFactory.php

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Topic::class, function (Faker $faker) {
   return [
       'name' => $faker->sentence(6),
-      'description' => $faker->paragraph(15),
+//      'description' => $faker->paragraph(15),
       'parent_id' => rand(2, 6),
   ];
 });
