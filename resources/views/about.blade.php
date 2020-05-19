fd3fbc9c7a0fea8b48a4690cea251971eca96764
Abdukhaligov <hikmat.pou@gmail.com>
2020-05-19 14:45:15 +0400

    bugfix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -31,45 +31,6 @@
     </div>
 
 
-  <!-- Team Section-->
-  <section class="our-team ot-inner" style="display:none;">
-    <div class="ot-top">
-      <span>Our Team</span>
-      <h1>People Behind the World </h1>
-      <h1>Digital Conference</h1>
-    </div>
-    <div class="container">
-      <div class="row">
-        @foreach(json_decode($data->team) as $member)
-          <div class="col-md-4">
-            <div class="single-team-member">
-              <div class="stm-img wow fadeInUp" data-wow-delay=".3s">
-                <a><img src="{{ Storage::disk('public')->url($member->attributes->photo) }}" alt=""></a>
-                <div class="stm-icon">
-                  @if($member->attributes->social_networks)
-                    <ul>
-                      @foreach($member->attributes->social_networks as $network)
-                        <li>
-                          <a target="_blank" href="{{ $network->attributes->link }}">
-                            <i class="fab {{ $network->attributes->network }}"></i>
-                          </a>
-                        </li>
-                      @endforeach
-                    </ul>
-                  @endif
-                </div>
-              </div>
-              <div class="stm-text wow fadeInDown" data-wow-delay=".5s">
-                <h4>{{ $member->attributes->name }}</h4>
-                <p>{{ $member->attributes->job_title }}</p>
-              </div>
-            </div>
-          </div>
-        @endforeach
-      </div>
-    </div>
-  </section>
-  <!-- /Team Section-->
 
 
 @endsection
